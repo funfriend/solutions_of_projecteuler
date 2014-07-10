686063f65588dcd73f9755a7cd5f664fb5492f7e
Cao Jiafeng <funfriendcjf@gmail.com>
2014-07-10 11:46:58 +0800

    Tree of primitive Pythagorean triples to generate all primitive triples

--- 75-singular-integer-right-triangles/readme.md
+++ 75-singular-integer-right-triangles/readme.md
@@ -11,3 +11,11 @@ Use Euclid's formula, which is
 Euclid's formula can generate all primitive Pythagorean triples but not all Pythagorean triples.
 In order to get all Pythagorean triples, multiply primitive Pythagorean triple by a integer k.
 (that's because any Pythagorean triple is either primitive or a multiple of another primitive Pythagorean triple)
+
+### Update: Tree of primitive Pythagorean triples
+
+I have transformed the code using
+[Tree of primitive Pythagorean triples](https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples)
+to generate primitive Pythagorean triples instead of original `gcd(m,n)` and `(m-n).odd?` test.
+
+However, the method seems to be slower, because of the frequent queue operation.
--- 75-singular-integer-right-triangles/singular_integer_right_triangles.rb
+++ 75-singular-integer-right-triangles/singular_integer_right_triangles.rb
@@ -1,19 +1,41 @@
+# help method
+inner_product = lambda { |v1, v2|
+  raise if v1.size != v2.size
+
+  v1.zip(v2).reduce(0) { |memo, p| memo += p[0] * p[1] }
+}
+
 l = 150_0000
 
 cache = Hash.new
-(2..Math.sqrt(l/2).floor).each do |m|
-  (1...m).each do |n|
-    if m.gcd(n) == 1 && (m-n).odd?
-      # calculate (a, b, c)
-      a, b, c = m**2 - n**2, 2 * m * n, m**2 + n**2
-      d = base = a + b + c
-      until d > l
-        # update cache
-        cache[d] ||= 0
-        cache[d] += 1
-        d += base
-      end
-    end
+
+# linear transformation matrix
+transformation = [
+  [[-1, 2, 2], [-2, 1, 2], [-2, 2, 3]],
+  [[1, 2, 2], [2, 1, 2], [2, 2, 3]],
+  [[1, -2, 2], [2, -1, 2], [2, -2, 3]]
+]
+
+# keep triple tree search elems, and seed the queue
+queue = Array.new
+queue.push [3, 4, 5]
+
+until queue.empty?
+  triple = queue.shift # pop queue
+  base = triple.reduce(:+)
+  # push queue
+  if base <= l
+    x, y, z = transformation.map { |t|
+      t.map { |v| inner_product.call(v, triple) }
+    }
+    queue.push(x, y, z)
+  end
+  # update cache
+  d = base
+  until d > l
+    cache[d] ||= 0
+    cache[d] += 1
+    d += base
   end
 end
 
